f1bbdbf94b66fb92be8a4475c9cd7f3f7aefbbc5
Erik Junghahn <erik.junghahn@gmail.com>
2023-11-29 23:38:10 +0100

    new hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,236 +1,30 @@
-import { Component, ElementRef, HostListener, ViewChild ,OnInit} from '@angular/core';
-import { AstMemoryEfficientTransformer } from '@angular/compiler';
-import * as $ from 'jquery';
-import { TweenMax, Power3 } from 'gsap';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+  OnInit,
+  Renderer2,
+} from "@angular/core";
+import { AstMemoryEfficientTransformer } from "@angular/compiler";
+import * as $ from "jquery";
+import "jquery"; // Import jQuery globally
 
+import { TweenMax, Power3 } from "gsap";
 
 declare function Di(): any;
 @Component({
-  selector: 'app-hero',
-  templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss'],
+  selector: "app-hero",
+  templateUrl: "./hero.component.html",
+  styleUrls: ["./hero.component.scss"],
 })
-export class HeroComponent  {
-    slideshowDuration: number = 4000;
- slideshow: JQuery<HTMLElement>=$('.main-content .slideshow');
-
- slideshowSwitch(slideshow: JQuery<HTMLElement>,index : number,auto: boolean) : void{
-  if(slideshow.data('wait')) return;
-
-  var slides = slideshow.find('.slide');
-  var pages = slideshow.find('.pagination');
-  var activeSlide = slides.filter('.is-active');
-  var activeSlideImage = activeSlide.find('.image-container');
-  var newSlide = slides.eq(index);
-  var newSlideImage = newSlide.find('.image-container');
-  var newSlideContent = newSlide.find('.slide-content');
-  var newSlideElements=newSlide.find('.caption > *');
-  if(newSlide.is(activeSlide))return;
-
-  newSlide.addClass('is-new');
-  var timeout=slideshow.data('timeout');
-  clearTimeout(timeout);
-  slideshow.data('wait',true);
-  var transition=slideshow.attr('data-transition');
-  if(transition=='fade'){
-    newSlide.css({
-      display:'block',
-      zIndex:2
-    });
-    newSlideImage.css({
-      opacity:0
-    });
-
-    TweenMax.to(newSlideImage,1,{
-      alpha:1,
-      onComplete:function(){
-        newSlide.addClass('is-active').removeClass('is-new');
-        activeSlide.removeClass('is-active');
-        newSlide.css({display:'',zIndex:''});
-        newSlideImage.css({opacity:''});
-        slideshow.find('.pagination').trigger('check');
-        slideshow.data('wait',false);
-        if(auto){
-          timeout=setTimeout(function(){
-            slideshowNext(slideshow,false,true);
-          },4000);
-          slideshow.data('timeout',timeout);}}});
-  } else {
-    var slideWidth = slideshow.width() || 1
-    if(newSlide.index()>activeSlide.index() && slideshow.width !== undefined){
-      var newSlideRight:any=0;
-      var newSlideLeft:any='auto';
-      var newSlideImageRight:any= -slideWidth/8;
-      var newSlideImageLeft:any='auto';
-      var newSlideImageToRight:any=0;
-      var newSlideImageToLeft:any='auto';
-      var newSlideContentLeft:any='auto';
-      var newSlideContentRight:any=0;
-      var activeSlideImageLeft:any=-slideWidth/4;
-    } else {
-      var newSlideRight:any='';
-      var newSlideLeft:any=0;
-      var newSlideImageRight:any='auto';
-      var newSlideImageLeft:any=-slideWidth/8;
-      var newSlideImageToRight:any='';
-      var newSlideImageToLeft:any=0;
-      var newSlideContentLeft:any=0;
-      var newSlideContentRight:any='auto';
-      var activeSlideImageLeft:any=slideWidth/4;
+export class HeroComponent {
+  constructor(private el: ElementRef) {}
+  scrollToSection(targetSectionId: string) {
+    const targetEl =
+      this.el.nativeElement.ownerDocument.getElementById(targetSectionId);
+    if (targetEl) {
+      targetEl.scrollIntoView({ behavior: "smooth" });
     }
-
-    newSlide.css({
-      display:'block',
-      width:0,
-      right:newSlideRight,
-      left:newSlideLeft
-      ,zIndex:2
-    });
-
-    newSlideImage.css({
-      width:slideWidth,
-      right:newSlideImageRight,
-      left:newSlideImageLeft
-    });
-
-    newSlideContent.css({
-      width:slideWidth,
-      left:newSlideContentLeft,
-      right:newSlideContentRight
-    });
-
-    activeSlideImage.css({
-      left:0
-    });
-
-    TweenMax.set(newSlideElements,{y:20,force3D:true});
-    TweenMax.to(activeSlideImage,1,{
-      left:activeSlideImageLeft,
-      ease:Power3.easeInOut
-    });
-
-    TweenMax.to(newSlide,1,{
-      width:slideshow.width(),
-      ease:Power3.easeInOut
-    });
-
-    TweenMax.to(newSlideImage,1,{
-      right:newSlideImageToRight,
-      left:newSlideImageToLeft,
-      ease:Power3.easeInOut
-    });
-
-  //   TweenMax.staggerFromTo(newSlideElements,0.8,{alpha:0,y:60},{alpha:1,y:0,ease:Power3.easeOut,force3D:true,delay:0.6},0.1,function(){
-  //     newSlide.addClass('is-active').removeClass('is-new');
-  //     activeSlide.removeClass('is-active');
-  //     newSlide.css({
-  //       display:'',
-  //       width:'',
-  //       left:'',
-  //       zIndex:''
-  //     });
-
-  //     newSlideImage.css({
-  //       width:'',
-  //       right:'',
-  //       left:''
-  //     });
-
-  //     newSlideContent.css({
-  //       width:'',
-  //       left:''
-  //     });
-
-  //     newSlideElements.css({
-  //       opacity:'',
-  //       transform:''
-  //     });
-
-  //     activeSlideImage.css({
-  //       left:''
-  //     });
-
-  //     slideshow.find('.pagination').trigger('check');
-  //     slideshow.data('wait',false);
-  //     if(auto){
-  //       timeout=setTimeout(function(){
-  //         slideshowNext(slideshow,false,true);
-  //       },slideshowDuration);
-  //       slideshow.data('timeout',timeout);
-  //     }
-  //   });
-  // }
-}
-
- slideshowNext(slideshow: JQuery<HTMLElement>,previous,auto):void{
-  var slides=slideshow.find('.slide');
-  var activeSlide=slides.filter('.is-active');
-  var newSlide=null;
-  if(previous){
-    newSlide=activeSlide.prev('.slide');
-    if(newSlide.length === 0) {
-      newSlide=slides.last();
-    }
-  } else {
-    newSlide=activeSlide.next('.slide');
-    if(newSlide.length==0)
-      newSlide=slides.filter('.slide').first();
   }
-
-  slideshowSwitch(slideshow,newSlide.index(),auto);
-}
-
-function homeSlideshowParallax(){
-  var scrollTop=$(window).scrollTop();
-  if(scrollTop>windowHeight) return;
-  var inner=slideshow.find('.slideshow-inner');
-  var newHeight=windowHeight-(scrollTop/2);
-  var newTop=scrollTop*0.8;
-
-  inner.css({
-    transform:'translateY('+newTop+'px)',height:newHeight
-  });
-}
-
-$(document).ready(function() {
- $('.slide').addClass('is-loaded');
-
- $('.slideshow .arrows .arrow').on('click',function(){
-  slideshowNext($(this).closest('.slideshow'),$(this).hasClass('prev'));
-});
-
- $('.slideshow .pagination .item').on('click',function(){
-  slideshowSwitch($(this).closest('.slideshow'),$(this).index());
-});
-
- $('.slideshow .pagination').on('check',function(){
-  var slideshow=$(this).closest('.slideshow');
-  var pages=$(this).find('.item');
-  var index=slideshow.find('.slides .is-active').index();
-  pages.removeClass('is-active');
-  pages.eq(index).addClass('is-active');
-});
-
-/* Lazyloading
-$('.slideshow').each(function(){
-  var slideshow=$(this);
-  var images=slideshow.find('.image').not('.is-loaded');
-  images.on('loaded',function(){
-    var image=$(this);
-    var slide=image.closest('.slide');
-    slide.addClass('is-loaded');
-  });
-*/
-
-var timeout=setTimeout(function(){
-  slideshowNext(slideshow,false,true);
-},slideshowDuration);
-
-slideshow.data('timeout',timeout);
-});
-
-if($('.main-content .slideshow').length > 1) {
-  $(window).on('scroll',homeSlideshowParallax);
-}
-
 }
